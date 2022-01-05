f6701c52337d97a659eb19abc1ad7910ecf1d5a0
Manoj Singh <manojsinghrana467@gmail.com>
2022-01-05 12:08:40 +0530

    Complete UI for Movie App

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ class Navbar extends React.Component {
     return (
       <div className="nav">
         <div className="search-container">
-          <input type="text" />
+          <input  />
           <button id="search-btn">Search</button>
         </div>
       </div>

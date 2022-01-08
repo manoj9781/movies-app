2ea64c4ab6cbbc8c16cb8e91a299c57298548358
Manoj Singh <manojsinghrana467@gmail.com>
2022-01-08 11:54:53 +0530

    Add Store to the project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import App from './Components/App';
 import './index.css'
+import movies from './reducers';
+
+
+const store = createStore(movies);
+console.log("store", store);
+console.log("State", store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
